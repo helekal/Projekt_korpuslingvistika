cd6193af0226d4b6a25de364b15755d6e00e1270
helekal <kaljumae.helen@gmail.com>
2017-01-09 16:02:49 +0200

    Delete tahendus_mis_arv.sh

--- Tähendus_mis/tahendus_mis_arv.sh
+++
@@ -1,7 +0,0 @@
-#! /bin/bash
-
-wc -l
-
-#sellele skriptile eelnevalt on kõik ühe murde keelejuhtide loendid ühendatud
-# üheks failiks käsuga cat *.txt > tahendus_mis_murre.txt
-#3. rida: loeb kokku failis olevad read ehk tähendus=mis märgendite arvu ühes murdes
